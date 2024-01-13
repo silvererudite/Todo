f7109508dc48f54bfa9f50ddb7e432759f19bb67
shamima19 <shamima.alma@bkash.com>
2024-01-13 17:00:17 +0600

    Init project structure

---
+++ src/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+
+    private String taskName;
+    private int taskId;
+    private String taskType;
+    public Task(String taskName, String taskType){
+        this.taskName = taskName;
+        this.taskType = taskType;
+    }
+
+}
